fd4cba5b8fe584f275840c5d1224709cd889cd16
Pablo Kaniefsky <pablokan@gmail.com>
2022-08-11 21:31:02 -0300

    first

---
+++ main.py
@@ -0,0 +1,5 @@
+from platform import python_branch
+import pywhatkit
+
+#pywhatkit.sendwhats_image("grupow", "ru.jpeg", "Desde programita de prueba en Python")
+pywhatkit.sendwhatmsg_to_group_instantly("grupow", "Desde programita de prueba en Python")
